5300d90563c31c5b1a18bb559bcdb59ba4b9400a
Vinny Principe <vprincipe@gmail.com>
2019-05-17 13:53:27 -0400

    updated controllers, server, models and orm

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,13 +1,38 @@
 var express = require("express");
 var router = express.Router();
 var router = require("express").Router()
-var burgerModel = require("../models/burgerModel")
+var burgerModel = require("../models/burger_Model")
 
-router.get("/", function(req, res){
-    burgerModel.all(function(displayData){
-        res.json(displayData)
 
-    })
-})
+
+router.get('/', function(req, res) {
+  burgerModel.selectAll(function(data) {
+      var hbsObject = {
+        burgerModel: data
+      };
+      // console.log(hbsObject);
+      res.render('index', hbsObject);
+    });
+  });
+  
+  router.post('/burgers', function(req, res) {
+    burgerModel.insertOne([
+      'burger_name'
+    ], [
+      req.body.burger_name
+    ], function(data) {
+      res.redirect('/');
+    });
+  });
+  
+  router.put('/burgers/:id', function(req, res) {
+    var condition = 'id = ' + req.params.id;
+  
+    burgerModel.updateOne({
+      devoured: true
+    }, condition, function(data) {
+      res.redirect('/');
+    });
+  });
 
 module.exports = router;
--- models/burger_model.js
+++ models/burger_model.js
@@ -1,11 +1,21 @@
-var orm = require("../config/orm");
+var orm = require("../config/orms");
 
 var burgerModel = {
     all: function(callBackModel){
         orm.all("burgers", function(data){
             callBackModel(data)
         })
-    }
+    },
+    update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
+          cb(res);
+        });
+      },
+      delete: function(condition, cb) {
+        orm.delete("burgers", condition, function(res) {
+          cb(res);
+        });
+      }
 }
 
 module.exports = burgerModel
